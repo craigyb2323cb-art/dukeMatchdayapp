4048f46ca51117ed9c99c3e73a8d7607147e868a
craigyb2323cb-art <craigyb2323.cb@gmail.com>
2026-03-06 14:52:59 +0000

    Update fixtures.jsx

--- src/fixtures.jsx
+++ src/fixtures.jsx
@@ -1,1 +1,20 @@
+import React from "react";
 
+export default function Fixtures() {
+
+return (
+
+<div style={{height:"100vh"}}>
+
+<iframe
+src="https://business.fanzo.com/fixtures"
+width="100%"
+height="100%"
+style={{border:"none"}}
+/>
+
+</div>
+
+);
+
+}
